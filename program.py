ee86377c39b21492028eb9f25eeb6b6fafaae86a
Mauricio Santana Bispo <mauricio.bispo@ufvjm.edu.br>
2020-09-22 20:31:26 -0300

    Primeiro programa Hello World

---
+++ program.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 18:48:43 2020
+
+@author: Mauricio
+@Revisao 1 - IA - 2020/5
+
+"""
+
+# Firt Program with Python - Hello World
+
+print("Hello World")
